0bb0eeb8f1bd7d6deededd60681021e04f30cf86
Mena <bluemagicuk@yahoo.co.uk>
2022-11-03 11:44:35 +0000

    Replace dummy text with typed mission details on FeatureTile cards

--- components/FeatureTile.tsx
+++ components/FeatureTile.tsx
@@ -1,12 +1,18 @@
+import TileImage from './atoms/TileImage';
 import TileText from './molecules/TileText';
 import styles from '../styles/FeatureTile.module.scss';
-import TileImage from './atoms/tileImage';
+import { launchMission } from '../mission';
 
-const FeatureTile = ({ src, alt }) => {
+const FeatureTile = ({ ...props }: launchMission) => {
   return (
     <div className={styles.wrapper}>
-      <TileImage src={src} alt={alt}></TileImage>
-      <TileText />
+      <TileImage src={props.links.mission_patch_small} alt={'rocket patch'}></TileImage>
+      <TileText
+        {...(props as Pick<
+          launchMission,
+          'launch_date_utc' | 'launch_failure_details' | 'rocket' | 'launch_success' | 'mission_name'
+        >)}
+      />
     </div>
   );
 };
